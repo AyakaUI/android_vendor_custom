e6104ad686d9738676c68e8647c1fb1b3f372f0a
hridaya <hridaya@pixelos.net>
2025-10-12 12:50:54 +0545

    config: fix ro.custom.build.date
    Change-Id: I810b45f4a29faffd78e1de65a4ce9f3800c90d07

--- config/version.mk
+++ config/version.mk
@@ -7,7 +7,7 @@ CUSTOM_VERSION_PROP := sixteen
 
 # PixelOS Platform Version
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.custom.build.date=$(BUILD_DATE) \
+    ro.custom.build.date=$(CUSTOM_BUILD_DATE) \
     ro.custom.device=$(CUSTOM_BUILD) \
     ro.custom.fingerprint=$(ROM_FINGERPRINT) \
     ro.custom.version=$(CUSTOM_VERSION)
