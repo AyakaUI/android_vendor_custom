a2d32f752ee7a821d30a711d346bd96406631ea1
Barry <barryofc11@gmail.com>
2026-01-27 23:46:04 +0000

    flavor: initial commit
    Change-Id: Icdb2052e647477805b18aae73ce45b7e6a404e3a
    Signed-off-by: Barry <barryofc11@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -9,7 +9,20 @@ CUSTOM_VERSION_PROP := sixteen
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.custom.build.date=$(CUSTOM_BUILD_DATE) \
     ro.custom.device=$(CUSTOM_BUILD) \
-    ro.custom.version=PixelOS_$(CUSTOM_VERSION)
+    ro.custom.version=Flavor_$(CUSTOM_VERSION)
+
+# Versioning System
+FLAVOR_BUILD_DATETIME := $(shell date +%s)
+FLAVOR_BUILD_DATE := $(shell date -d @$(FLAVOR_BUILD_DATETIME) +"%Y%m%d-%H%M%S")
+
+TARGET_PRODUCT_SHORT := $(subst custom_,,$(CUSTOM_BUILD))
+
+FLAVOR_BUILD_TYPE ?= OSS
+FLAVOR_BUILD_VERSION := BP3A
+FLAVOR_VERSION := $(FLAVOR_BUILD_VERSION)-$(FLAVOR_BUILD_TYPE)-$(TARGET_PRODUCT_SHORT)-$(FLAVOR_BUILD_DATE)
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+  ro.flavor.version=$(FLAVOR_VERSION) \
 
 # Updater
 ifeq ($(IS_OFFICIAL),true)
